6801da5ec32c0d36856aa74e640b7bf8d1c46b94
shaan Yadav <shaanyworkspace@gmail.com>
2025-03-25 18:17:18 +0530

    Added a security password bcryption and encryption

--- src/main/java/com/employee/Service/EmployeeDetailService.java
+++ src/main/java/com/employee/Service/EmployeeDetailService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,7 +17,10 @@ import java.util.List;
 public class EmployeeDetailService implements UserDetailsService {
     private final EmployeeDetailRepo employeeDetailRepo;
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
     public EmployeeDetails AddEmployee(EmployeeDetails employeeDetails){
+        employeeDetails.setPassword(bCryptPasswordEncoder.encode(employeeDetails.getPassword()));
         return employeeDetailRepo.save(employeeDetails);
     }
 
